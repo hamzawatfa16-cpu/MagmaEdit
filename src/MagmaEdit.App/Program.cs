e573c4cc66e0c3a7a78a301a976c437d9ef897b3
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-02 22:55:04 +0100

    Harden MagmaEdit startup failure reporting

--- src/MagmaEdit.App/Program.cs
+++ src/MagmaEdit.App/Program.cs
@@ -5,7 +5,18 @@ namespace MagmaEdit.App;
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static int Main(string[] args)
+    {
+        try
+        {
+            return BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception exception)
+        {
+            StartupDiagnostics.Write(exception);
+            return 1;
+        }
+    }
 
     private static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>()
