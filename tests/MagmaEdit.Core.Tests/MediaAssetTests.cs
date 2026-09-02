b6c918650fbdf7a40d3893c84f25a56947c85c89
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-03 00:51:33 +0100

    Test reversible media renaming

--- tests/MagmaEdit.Core.Tests/MediaAssetTests.cs
+++ tests/MagmaEdit.Core.Tests/MediaAssetTests.cs
@@ -1,3 +1,4 @@
+using MagmaEdit.Core.Editing;
 using MagmaEdit.Core.Media;
 
 namespace MagmaEdit.Core.Tests;
@@ -37,5 +38,41 @@ public sealed class MediaAssetTests
         Assert.Equal(1920, asset.Metadata.Height);
         Assert.Equal(30, asset.Metadata.FramesPerSecond);
         Assert.Equal("h264", asset.Metadata.VideoCodec);
+    }
+
+    [Fact]
+    public void RenameChangesOnlyProjectDisplayNameAndCanBeReverted()
+    {
+        MediaAsset asset = MediaAsset.Create("C:\\source\\clip.mp4", "C:\\library\\clip.mp4");
+        var assets = new List<MediaAsset> { asset };
+        var command = new RenameMediaAssetCommand(assets, asset.Id, "My Short.mp4");
+
+        command.Apply();
+
+        Assert.Equal("My Short.mp4", assets[0].FileName);
+        Assert.Equal(asset.Id, assets[0].Id);
+        Assert.Equal(asset.SourcePath, assets[0].SourcePath);
+        Assert.Equal(asset.LibraryPath, assets[0].LibraryPath);
+
+        command.Revert();
+
+        Assert.Equal("clip.mp4", assets[0].FileName);
+        Assert.Equal(asset.SourcePath, assets[0].SourcePath);
+        Assert.Equal(asset.LibraryPath, assets[0].LibraryPath);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(".")]
+    [InlineData("..")]
+    [InlineData("bad/name.mp4")]
+    [InlineData("bad\\name.mp4")]
+    public void RenameRejectsInvalidDisplayNames(string name)
+    {
+        MediaAsset asset = MediaAsset.Create("C:\\source\\clip.mp4", "C:\\library\\clip.mp4");
+        var assets = new List<MediaAsset> { asset };
+
+        Assert.ThrowsAny<ArgumentException>(() => new RenameMediaAssetCommand(assets, asset.Id, name));
     }
 }
